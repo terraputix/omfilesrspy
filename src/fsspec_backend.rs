9a045419dc2a8bc5daa0eea2b54e73bc90dda215
terraputix <terraputix@mailbox.org>
2024-12-17 18:24:59 +0100

    fsspec caching implementation

--- src/fsspec_backend.rs
+++ src/fsspec_backend.rs
@@ -10,11 +10,40 @@ pub struct FsSpecBackend {
     file_size: u64,
 }
 
+/// Different types of caches that can be used with the caching filesystem
+/// Compare: https://filesystem-spec.readthedocs.io/en/latest/api.html#fsspec.implementations.cached.CachingFileSystem
+pub enum CacheType {
+    /// Caches whole remote files on first access
+    SimpleCacheFileSystem,
+    /// Chunkwise local storage in a sparse file
+    CachingFileSystem,
+    /// Caches whole remote files on first access, similar to CachingFileSystem
+    /// but without requiring a sparse file
+    WholeFileCacheFileSystem,
+}
+
+impl CacheType {
+    pub fn to_str(&self) -> &str {
+        match self {
+            CacheType::SimpleCacheFileSystem => "SimpleCacheFileSystem",
+            CacheType::CachingFileSystem => "CachingFileSystem",
+            CacheType::WholeFileCacheFileSystem => "WholeFileCacheFileSystem",
+        }
+    }
+}
+
+pub struct CacheSettings {
+    pub cache_dir: PathBuf,
+    pub expiry_time: Option<u64>,
+    pub cache_type: CacheType,
+}
+
 impl FsSpecBackend {
     pub fn new(
         path: &str,
         protocol: Option<&str>,
         kwargs: Option<&Bound<'_, PyDict>>,
+        cache_settings: Option<CacheSettings>,
     ) -> PyResult<Self> {
         Python::with_gil(|py| {
             // Convert path to absolute path if it's a local file
@@ -30,15 +59,36 @@ impl FsSpecBackend {
                 path_buf
             };
             // Determine protocol and path
-            let protocol = protocol.map_or("file", |p| p);
+            let protocol = protocol.unwrap_or("file");
             let final_path = absolute_path.to_string_lossy().to_string();
 
-            // Create fs and "open" the file
+            // Create filesystem without cache
             let fsspec = py.import("fsspec")?;
             let fs = fsspec.call_method("filesystem", (protocol,), kwargs)?;
-            let py_file = fs.call_method("open", (final_path,), None)?;
-            // Get file size
-            let info = fs.call_method("info", (path,), None)?;
+
+            // Potentially overwrite filesystem with caching filesystem
+            let fs = if let Some(cache_settings) = cache_settings {
+                let cached_fs = py
+                    .import("fsspec.implementations.cached")?
+                    .getattr(cache_settings.cache_type.to_str())?;
+
+                // Create cache filesystem options
+                let cache_options = PyDict::new(py);
+                cache_options.set_item("fs", fs)?;
+                cache_options
+                    .set_item("cache_storage", cache_settings.cache_dir.to_str().unwrap())?;
+                if let Some(expiry) = cache_settings.expiry_time {
+                    cache_options.set_item("expiry_time", expiry)?;
+                }
+                // Return the caching filesystem
+                cached_fs.call((), Some(&cache_options))?
+            } else {
+                fs
+            };
+
+            // "open" the file and get the file size
+            let py_file = fs.call_method("open", (final_path.clone(),), None)?;
+            let info = fs.call_method("info", (final_path,), None)?;
             let size = info.get_item("size")?.extract::<u64>()?;
 
             Ok(Self {
@@ -102,7 +152,7 @@ mod tests {
     fn test_fsspec_backend() -> Result<(), Box<dyn Error>> {
         pyo3::prepare_freethreaded_python();
 
-        let file = FsSpecBackend::new("test_files/read_test.om", Some("file"), None)?;
+        let file = FsSpecBackend::new("test_files/read_test.om", Some("file"), None, None)?;
         assert_eq!(file.file_size, 144);
 
         let bytes = file.get_bytes_owned(0, 44)?;
@@ -129,6 +179,7 @@ mod tests {
                 "openmeteo/data/dwd_icon_d2/temperature_2m/chunk_3980.om",
                 Some("s3"),
                 Some(&kwargs),
+                None,
             )?;
 
             assert_eq!(file.file_size, 34479300);
@@ -162,6 +213,58 @@ mod tests {
                 "openmeteo/data/dwd_icon_d2/temperature_2m/chunk_3960.om",
                 Some("s3"),
                 Some(&kwargs),
+                None,
+            )?;
+
+            // Create OmFileReader with the FsSpecBackend
+            let reader = OmFileReader2::new(Arc::new(backend), 256)
+                .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+
+            // Read a small section of data
+            let ranges = vec![
+                Range {
+                    start: 57812,
+                    end: 57813,
+                },
+                Range { start: 0, end: 100 },
+            ];
+
+            let data = reader
+                .read_simple(&ranges, None, None)
+                .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+
+            // Verify we got some data
+            assert_eq!(
+                &data[..10],
+                &[18.0, 17.7, 17.65, 17.45, 17.15, 17.6, 18.7, 20.75, 21.7, 22.65]
+            );
+
+            Ok(())
+        })?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_s3_reader_with_cache() -> Result<(), Box<dyn Error>> {
+        pyo3::prepare_freethreaded_python();
+
+        Python::with_gil(|py| -> Result<(), Box<dyn Error>> {
+            let kwargs = PyDict::new(py);
+            kwargs.set_item("anon", true)?;
+
+            let cache_settings = CacheSettings {
+                cache_dir: PathBuf::from("/tmp/fsspec_cache"),
+                expiry_time: None, // Use Python default expiry time
+                cache_type: CacheType::SimpleCacheFileSystem,
+            };
+
+            // Initialize the FsSpecBackend with cache
+            let backend = FsSpecBackend::new(
+                "openmeteo/data/dwd_icon_d2/temperature_2m/chunk_3960.om",
+                Some("s3"),
+                Some(&kwargs),
+                Some(cache_settings),
             )?;
 
             // Create OmFileReader with the FsSpecBackend
