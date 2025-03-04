9b0e1d83158cc7c36f427b4ef781ffbff9f6da53
terraputix <terraputix@mailbox.org>
2025-03-04 12:26:22 +0100

    fix types for older python versions

--- python/omfilesrspy/types.py
+++ python/omfilesrspy/types.py
@@ -1,5 +1,9 @@
-from types import EllipsisType
+try:
+    from types import EllipsisType
+except ImportError:
+    EllipsisType = type(Ellipsis)
+from typing import Tuple, Union
 
-# This is from https://github.com/zarr-developers/zarr-python/blob/main/src/zarr/core/indexing.py#L38C1-L40C87
-BasicSelector = int | slice | EllipsisType
-BasicSelection = BasicSelector | tuple[BasicSelector, ...]  # also used for BlockIndex
+# This is similar to https://github.com/zarr-developers/zarr-python/blob/main/src/zarr/core/indexing.py#L38C1-L40C87
+BasicSelector = Union[int, slice, EllipsisType]
+BasicSelection = Union[BasicSelector, Tuple[Union[int, slice, EllipsisType], ...]]  # also used for BlockIndex
