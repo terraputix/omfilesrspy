2de725b1cc8386ab2d242d0d03ea7fc16092f603
terraputix <terraputix@mailbox.org>
2025-03-26 17:44:51 +0100

    fix arg parser for tuples

--- benchmarks/helpers/args.py
+++ benchmarks/helpers/args.py
@@ -5,10 +5,10 @@ from omfiles.types import BasicSelection
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Benchmark different file formats")
-    parser.add_argument("--array-size", type=tuple, default=(10, 10, 1, 10, 10, 10), help="Size of the test array")
-    parser.add_argument("--chunk-size", type=tuple, default=(10, 5, 1, 1, 1, 1), help="Chunk size for writing data")
+    parser.add_argument("--array-size", type=parse_tuple, default=(10, 10, 1, 10, 10, 10), help="Size of the test array")
+    parser.add_argument("--chunk-size", type=parse_tuple, default=(10, 5, 1, 1, 1, 1), help="Chunk size for writing data")
     parser.add_argument("--read-index", type=parse_tuple, default=(0, 0, 0, 0, ...), help="Index for reading")
-    parser.add_argument("--iterations", type=int, default=5, help="Number of iterations for each test")
+    parser.add_argument("--iterations", type=int, default=10, help="Number of iterations for each test")
     return parser.parse_args()
 
 
