500c512f85b20e66415c046e62b76c015a2dfc7d
terraputix <terraputix@mailbox.org>
2025-03-27 13:04:36 +0100

    add failing test for xarray via fsspec on S3

--- tests/test_fsspec.py
+++ tests/test_fsspec.py
@@ -3,6 +3,8 @@ import os
 import fsspec
 import numpy as np
 import omfiles
+import pytest
+import xarray as xr
 
 from .test_utils import create_test_om_file
 
@@ -13,32 +15,40 @@ from .test_utils import create_test_om_file
 #     assert file.file_size == 144
 
 
-def test_s3_reader():
-    file_path = "openmeteo/data/dwd_icon_d2/temperature_2m/chunk_3960.om"
+@pytest.fixture
+def s3_backend():
+    s3_test_file = "openmeteo/data/dwd_icon_d2/temperature_2m/chunk_3960.om"
     fs = fsspec.filesystem("s3", anon=True)
-    backend = fs.open(file_path, mode="rb")
+    yield fs.open(s3_test_file, mode="rb")
 
-    # Create reader over fs spec backend
-    reader = omfiles.OmFilePyReader(backend)
-    data = reader[57812:57813, 0:100]
-
-    # Verify the data
-    expected = [18.0, 17.7, 17.65, 17.45, 17.15, 17.6, 18.7, 20.75, 21.7, 22.65]
-    np.testing.assert_array_almost_equal(data[:10], expected)
-
-
-def test_s3_reader_with_cache():
-    file_path = "openmeteo/data/dwd_icon_d2/temperature_2m/chunk_3960.om"
+@pytest.fixture
+def s3_backend_with_cache():
+    s3_test_file = "openmeteo/data/dwd_icon_d2/temperature_2m/chunk_3960.om"
     fs = fsspec.filesystem(protocol="s3", anon=True)
-    backend = fs.open(file_path, mode="rb", cache_type="mmap", block_size=1024, cache_options={"location": "cache"})
+    yield fs.open(s3_test_file, mode="rb", cache_type="mmap", block_size=1024, cache_options={"location": "cache"})
 
-    # Create reader over fs spec backend
-    reader = omfiles.OmFilePyReader(backend)
+
+def test_s3_reader(s3_backend):
+    reader = omfiles.OmFilePyReader(s3_backend)
     data = reader[57812:57813, 0:100]
 
-    # Verify the data
     expected = [18.0, 17.7, 17.65, 17.45, 17.15, 17.6, 18.7, 20.75, 21.7, 22.65]
     np.testing.assert_array_almost_equal(data[:10], expected)
+
+
+def test_s3_reader_with_cache(s3_backend_with_cache):
+    reader = omfiles.OmFilePyReader(s3_backend_with_cache)
+    data = reader[57812:57813, 0:100]
+
+    expected = [18.0, 17.7, 17.65, 17.45, 17.15, 17.6, 18.7, 20.75, 21.7, 22.65]
+    np.testing.assert_array_almost_equal(data[:10], expected)
+
+
+@pytest.mark.xfail(reason="Om Files on S3 currently have no names assigned for the variables")
+def test_s3_xarray(s3_backend_with_cache):
+    ds = xr.open_dataset(s3_backend_with_cache, engine="om")
+    assert any(ds.variables.keys())
+
 
 def test_fsspec_reader_close():
     """Test that closing a reader with fsspec file object works correctly."""
